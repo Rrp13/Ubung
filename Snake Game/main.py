1eb5652bb24860806a7d90d3e5c207c707ef4bdf
Prashant Subedi <prashantsubedi@pop-os.localdomain>
2020-03-28 21:04:10 +0545

    First version

--- Snake Game/main.py
+++ Snake Game/main.py
@@ -2,187 +2,135 @@ import pygame
 import math
 import random
 
+STARTING_SPEED = 10
+ACCELARATION = 1
+# You don't really have to use this image though
+# Can just draw a rect
+# Which would make collision detection really easy :D
+SNAKE_PART_ICON = pygame.image.load("snake.png")
+# Keeping this fixed for now
+ALL_OBJECTS_WIDTH = 32
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+FOOD_IMAGE = pygame.image.load("food.png")
+FPS = 30
+
+#score
 pygame.init()
-screen=pygame.display.set_mode((800,600))
+screen=pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 running=True
 icon=pygame.image.load("icon.png")
 pygame.display.set_caption("Snake")
 pygame.display.set_icon(icon)
-speed=0.5
 
-#score
 score_value=0
 font=pygame.font.Font("freesansbold.ttf",32)
 textX=10
 textY=10
 
+
+snake = [(10, 50)]
+speed = STARTING_SPEED
+
+velocity = (speed, 0)
+
 #gameover
-over_font=pygame.font.Font("freesansbold.ttf",60)
+over_font = pygame.font.Font("freesansbold.ttf",60)
 
 def show_score(x,y):
     score=font.render("Score: "+ str(score_value),True,(0,125,125))
     screen.blit(score,(x,y))
+
+
 def game_over_text():
     over_text=over_font.render("GAME OVER",True,(200,0,100))
     screen.blit(over_text,(210,250))
 
-#snake
-snakeImg=[]
-snakeX=[]
-snakeY=[]
-snakeX_change=[]
-snakeY_change=[]
-snakeImg.append(pygame.image.load("snake.png"))
-snakeX.append(10)
-snakeY.append(500)
-snakeX_change.append(speed)
-snakeY_change.append(0)
-#snakeImg.append(pygame.image.load("snake.png"))
-#snakeX.append(5)
-#snakeY.append(500)
-#snakeX_change.append(speed)
-#snakeY_change.append(0)
 
-#food
-foodImg=pygame.image.load("food.png")
-foodX=random.randint(15,750)
-foodY=random.randint(15,550)
+def generate_food_coordinates():
+    return (
+        random.randint(0, SCREEN_WIDTH - ALL_OBJECTS_WIDTH),
+        random.randint(0, SCREEN_HEIGHT - ALL_OBJECTS_WIDTH)
+    )
 
-def snake(x,y,i):
-    screen.blit(snakeImg[i],(x,y))
+
+def draw_snake_part(coordinates):
+    screen.blit(SNAKE_PART_ICON, coordinates)
     
 
-def food(x,y):
-    screen.blit(foodImg,(x,y))
+def draw_food(coordinates):
+    screen.blit(FOOD_IMAGE, coordinates)
 
-def boundry_check (x,y):
-    if x==0 or x== 770 or y==0 or y==660:
-        return True
-    else:
+
+def has_collided_with_boundary(snake_head):
+    return (
+            snake_head[0] < 0
+            or snake_head[0] + ALL_OBJECTS_WIDTH > SCREEN_WIDTH
+            or snake_head[1] < 0
+            or snake_head[1] + ALL_OBJECTS_WIDTH > SCREEN_HEIGHT
+    )
+
+
+def has_collided(snake_head, obstacle):
+    if snake_head[0] + ALL_OBJECTS_WIDTH < obstacle[0]:
         return False
-def distance(x,y,a,b):
-    return (math.sqrt(math.pow((x-a),2)+math.pow((y-b),2)))
-def isCollision():
-    if  distance(snakeX[0],snakeY[0],foodX,foodY) < 30:
-        return True
-    else:
+    if snake_head[0] > obstacle[0] + ALL_OBJECTS_WIDTH:
         return False
-def change():
-     for i in range(len(snakeImg)-1):
-        snakeX[i+1]=snakeX[i]
-        snakeY[i+1]=snakeY[i]-30
-        #snakeX_change[i+1]=snakeX_change[i]
-        #snakeY_change[i+1]=snakeY_change[i]
-#def change_2()
- #   for i in range(len(snakeImg)-1)
-  #   if snakeX[i+1]==snake[i] and if snakeY[i+1]==snakeY[i]
-   #     snakeX_change[]
+    if snake_head[1] + ALL_OBJECTS_WIDTH < obstacle[1]:
+        return False
+    if snake_head[1] > obstacle[1] + ALL_OBJECTS_WIDTH:
+        return False
+    return True
 
-def snake_add():
-    if len(snakeImg) > 1:
-        if (snakeY[-1]-snakeY[-2])==0 and (snakeX[-1]-snakeX[-2])>0:
-             snakeImg.append(pygame.image.load("snake.png"))
-             snakeX.append(snakeX[-1])
-             snakeY.append((snakeY[-1])-30)
-        if (snakeY[-1]-snakeY[-2])==0 and (snakeX[-1]-snakeX[-2])<0:
-             snakeImg.append(pygame.image.load("snake.png"))
-             snakeX.append(snakeX[-1])
-             snakeY.append((snakeY[-1])+30)
-        if (snakeX[-1]-snakeX[-2])==0 and (snakeY[-1]-snakeY[-2])>0:
-             snakeImg.append(pygame.image.load("snake.png"))
-             snakeX.append((snakeX[-1]-30))
-             snakeY.append((snakeY[-1]))
-        if (snakeX[-1]-snakeX[-2])==0 and (snakeY[-1]-snakeY[-2])<0:
-             snakeImg.append(pygame.image.load("snake.png"))
-             snakeX.append((snakeX[-1]+30))
-             snakeY.append((snakeY[-1]))
 
-    if len(snakeImg)==1:
-        if snakeX_change[0]==0 and snakeY_change[0]>0:
-             snakeImg.append(pygame.image.load("snake.png"))
-             snakeX.append(snakeX[-1])
-             snakeY.append((snakeY[-1])-30)
-             
-        if snakeX_change[0]==0 and snakeY_change[0]<0:
-             snakeImg.append(pygame.image.load("snake.png"))
-             snakeX.append(snakeX[-1])
-             snakeY.append((snakeY[-1])+30)
-             
-        if snakeY_change[0]==0 and snakeX_change[0]>0:
-             snakeImg.append(pygame.image.load("snake.png"))
-             snakeX.append(snakeX[-1]-30)
-             snakeY.append((snakeY[-1]))
-             
-        if snakeY_change[0]==0 and snakeX_change[0]<0:
-             snakeImg.append(pygame.image.load("snake.png"))
-             snakeX.append(snakeX[-1]+30)
-             snakeY.append((snakeY[-1]))
-             
+def move_snake(snake, direction, elongate):
+    new_snake = [move_snake_head(snake[0], direction)] + snake[:-1]
+    if elongate:
+        new_snake.append(snake[-1])
+    return new_snake
 
-    
-        
-#Game loop
+def move_snake_head(snake, direction):
+    return snake[0] + direction[0], snake[1] + direction[1]
+
+
+food = generate_food_coordinates()
+# Game loop
+clock = pygame.time.Clock()
+game_over = False
 while running:
+    if game_over:
+        # No need to render anything now
+        continue
     screen.fill((255,255,255))
-    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT and snakeY_change[0] != 0:
-                #change()
-                snakeX_change[0]=speed
-                snakeY_change[0]=0
-                
-            if event.key == pygame.K_LEFT and snakeY_change[0] != 0:
-                #change()
-                snakeX_change[0]=-speed
-                snakeY_change[0]=0
-                    
-            if event.key == pygame.K_UP and snakeX_change[0] != 0:
-                #change()
-                snakeX_change[0]=0
-                snakeY_change[0]=-speed
-                  
-                 
-            if event.key == pygame.K_DOWN and snakeX_change[0] != 0:
-                #change()
-                snakeX_change[0]=0
-                snakeY_change[0]=speed
-                
+            if event.key == pygame.K_RIGHT and velocity[1] != 0:
+                velocity = (speed, 0)
+            elif event.key == pygame.K_LEFT and velocity[1] != 0:
+                velocity = (-speed, 0)
+            elif event.key == pygame.K_UP and velocity[0] != 0:
+                velocity = (0, -speed)
+            elif event.key == pygame.K_DOWN and velocity[0] != 0:
+                velocity = (0, speed)
+
+    if has_collided_with_boundary(snake[0]):
+        game_over = True
+        game_over_text()
+
+    if has_collided(snake[0], food):
+        food = generate_food_coordinates()
+        score_value += 1
+        snake = move_snake(snake, velocity, True)
+        speed = speed + ACCELARATION
+    else:
+        snake = move_snake(snake, velocity, False)
+
+    for snake_part in snake:
+        draw_snake_part(snake_part)
 
 
-    
-    if boundry_check(snakeX[0],snakeY[0]):
-        for i in range(len(snakeImg)):
-        
-            snakeX_change[0]=0
-            snakeY_change[0]=0
-            game_over_text()
-      
-      
-    
-    if isCollision():
-      foodX=random.randint(15,750)
-      foodY=random.randint(15,550) 
-      score_value+=1
-      snake_add()
-      #change()
-    
-    snakeX[0]+=snakeX_change[0]
-    snakeY[0]+=snakeY_change[0]
-    
-    for j in range(len(snakeImg)):
-
-        #change()
-        snake(snakeX[j],snakeY[j],j)
-    #change()
-    
-    print(len(snakeImg))
-    print(snakeX[:])
-    print(snakeY[:])
-    show_score(textX,textY)
-    food(foodX,foodY)
-    #change()
+    draw_food(food)
     pygame.display.update()
-    #change()
+    clock.tick(FPS)
